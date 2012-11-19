57b08e73ed995db10ea37858da343f99d17b8ccd
jimmyzzz009 <jjj@jj.jjj>
2012-11-19 16:14:49 +0800

    jjj

---
+++ rrudp/src/com/hd123/auction/test/Server.java
@@ -0,0 +1,14 @@
+package com.hd123.auction.test;
+
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("hello world");
+
+	}
+
+}
