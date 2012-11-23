7cc44dd90fd444700b3e619f0abc75a884e4bd49
jimmyzzz009 <jjj@jj.jjj>
2012-11-23 21:01:27 +0800

    dd

--- rrudp/src/com/hd123/auction/UDTReceiver.java
+++ rrudp/src/com/hd123/auction/UDTReceiver.java
@@ -44,11 +44,7 @@ public class UDTReceiver {
 
 	//a variable to record number of continuous EXP time-out events 
 	private volatile long expCount=0;
-
-	/*records the time interval between each probing pair
-    compute the median packet pair interval of the last
-	16 packet pair intervals (PI) and the estimate link capacity.(packet/s)*/
-//	private final PacketPairWindow packetPairWindow;
+//	private final PacketPairWindow  packetPairWindow;
 
 	//estimated link capacity
 	long estimateLinkCapacity;
